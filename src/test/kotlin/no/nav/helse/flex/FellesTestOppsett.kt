8f9eca2e586fd42cf52c09573f4bb795cae19e4c
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2023-01-10 06:22:14 +0100

    Lag applikasjonsskjelett med database (#1)

---
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -0,0 +1,35 @@
+package no.nav.helse.flex
+
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.testcontainers.containers.PostgreSQLContainer
+import java.util.*
+
+private class PostgreSQLContainer14 : PostgreSQLContainer<PostgreSQLContainer14>("postgres:14-alpine")
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureObservability
+@SpringBootTest(classes = [Application::class])
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE, printOnlyOnFailure = false)
+abstract class FellesTestOppsett {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    companion object {
+
+        init {
+            PostgreSQLContainer14().apply {
+                start()
+                System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
+                System.setProperty("spring.datasource.username", username)
+                System.setProperty("spring.datasource.password", password)
+            }
+        }
+    }
+}
