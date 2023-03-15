36b343295daa21c946917a84d5bc47e0b3abd30e
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2023-03-15 11:31:34 +0100

    Bruk basepath (#12)

--- src/main/kotlin/no/nav/helse/flex/FeilmeldingApi.kt
+++ src/main/kotlin/no/nav/helse/flex/FeilmeldingApi.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/v1")
+@RequestMapping("/syk/feilmeldinger/api/v1")
 @RestController
 class FeilmeldingApi {
 
