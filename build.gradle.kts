e5b8d847bdea129f66f10f9d0aa6e2676a47bf82
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2023-04-20 11:55:59 +0200

    Prometheus (#22)
    * Export prometheus metrics
    
    * Implement prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.flywaydb:flyway-core")
     implementation("org.postgresql:postgresql")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
 
