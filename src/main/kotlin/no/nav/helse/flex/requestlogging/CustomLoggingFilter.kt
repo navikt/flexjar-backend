5aa6d80eca10d8ce136a938de170e283c5955027
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-06 11:27:27 +0100

    customfilter

---
+++ src/main/kotlin/no/nav/helse/flex/requestlogging/CustomLoggingFilter.kt
@@ -0,0 +1,26 @@
+package no.nav.helse.flex.requestlogging
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class CustomLoggingFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        filterChain.doFilter(request, response)
+
+        val requestUri = request.requestURI
+        if (requestUri.startsWith("/internal")) return
+        val requestId = request.getHeader("X-Request-ID")
+        val status = response.status
+
+        logger.info("${request.method} $requestUri, X-Request-ID: $requestId, Response Status: $status")
+    }
+}
