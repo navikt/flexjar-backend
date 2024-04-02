31c92e7a13dd41bebdb93abb36fa4c8092eec55a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-02 09:25:33 +0200

    Stjerne fiks (#128)

--- src/main/kotlin/no/nav/helse/flex/repository/PagingFeedbackRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/repository/PagingFeedbackRepository.kt
@@ -29,7 +29,7 @@ class PagingFeedbackRepository(
                     ""
                 } +
                 if (stjerne) {
-                    " AND tags like '%stjerne'"
+                    " AND tags like '%stjerne%'"
                 } else {
                     ""
                 } +
