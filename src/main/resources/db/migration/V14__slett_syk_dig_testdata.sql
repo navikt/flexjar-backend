149b423fb3e2b01029ec0390e991294283c3dbab
Karl J. Overå <k@rl.run>
2024-09-03 14:32:05 +0200

    chore: clean up some testdata in prod

---
+++ src/main/resources/db/migration/V14__slett_syk_dig_testdata.sql
@@ -0,0 +1,12 @@
+DELETE
+FROM feedback
+WHERE id IN (
+             'b793643f-6d32-4ca4-9985-80e42d5f46cc',
+             '4bf05918-dba8-4519-8d2b-a40bd268988c',
+             '44689119-185a-48ed-bb0a-837845bed0d2',
+             '0761c47b-5214-4114-bca4-220a88f58cec',
+             '8b79fe40-c040-46c6-9bab-99ede80cabb8',
+             'f6e749b7-1e0b-4012-a9a3-a65c6906abcb',
+             'f7cd026c-3046-4803-a5b4-63fb216eea2c',
+             '11a7b1b7-d2fc-458b-b6f4-150b3d26337f'
+    );
